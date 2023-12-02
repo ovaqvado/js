469e685b2d10f43551fb5b93318d4f446bd14423
ovaqvado <likeriklohhg@gmail.com>
2023-12-02 22:05:51 +0300

    1dz

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 class Product {
-    constructor(params = {}) {
-      Object.assign(this, {
-        pName: null, 
-        amount: 1, 
-        bought: false
-      }, { ...params }); 
-    }
-  
-    static sortDefault(a, b) {  // чтобы сначала шли некупленные продукты, а потом – купленные
-      return (a.bought - b.bought) * 10 + a.pName.localeCompare(b.pName); 
-    }; 
+  constructor(params = {}) {
+    Object.assign(this, {
+      pName: null, 
+      amount: 1, 
+      bought: false
+    }, { ...params }); 
   }
-  Product.prototype.toString = function () {
-    return `${this.pName} (x${this.amount}), ${this.bought ? 'куплено' : 'не куплено'}`;
+
+  static sortDefault(a, b) {  // чтобы сначала шли некупленные продукты, а потом – купленные
+    return (a.bought - b.bought) * 10 + a.pName.localeCompare(b.pName); 
   }; 
-  
-  
-  const shopList = [
-    { pName: 'Яблоко', amount: 2 },
-    { pName: 'Арбуз', amount: 5, bought: true },
-    { pName: 'Дыня', bought: true },
-    { pName: 'Груша', amount: 10 }
-  ].map(prodDef => new Product(prodDef));
-  
-  const addToShopList = prodDef => { 
-    const prod = shopList.find(prod => prod.pName === prodDef.pName); 
-    if (!prod) return shopList.push(new Product(prodDef)); 
-    prod.amount += prodDef.amount; 
-  }; 
-  const printShopList = () => shopList.forEach(
-    (prod, i) => console.log(`${i + 1}. ${prod}`)
-  ); 
-  
-  shopList.sort(Product.sortDefault);
-  printShopList();
-  console.log('---'); 
-  
-  addToShopList({ pName: 'Яблоко', amount: 3 });
-  addToShopList({ pName: 'Виноград', amount: 2 });
-  shopList.sort(Product.sortDefault);
-  printShopList();
+}
+Product.prototype.toString = function () {
+  return `${this.pName} (x${this.amount}), ${this.bought ? 'куплено' : 'не куплено'}`;
+}; 
+
+
+const shopList = [
+  { pName: 'Банан', amount: 2 },
+  { pName: 'Апельсин', amount: 5, bought: true },
+  { pName: 'Молоко', bought: true },
+  { pName: 'Груша', amount: 10 }
+].map(prodDef => new Product(prodDef));
+
+const addToShopList = prodDef => {  // при добавлении покупки с уже существующим в списке продуктом, необходимо увеличивать количество в существующей покупке, а не добавлять новую
+  const prod = shopList.find(prod => prod.pName === prodDef.pName); 
+  if (!prod) return shopList.push(new Product(prodDef)); 
+  prod.amount += prodDef.amount; 
+}; 
+const printShopList = () => shopList.forEach(
+  (prod, i) => console.log(`${i + 1}. ${prod}`)
+); 
+
+shopList.sort(Product.sortDefault);
+printShopList();
+console.log('---'); 
+
+addToShopList({ pName: 'Банан', amount: 3 });
+addToShopList({ pName: 'Шоколад', amount: 2 });
+shopList.sort(Product.sortDefault);
+printShopList();
